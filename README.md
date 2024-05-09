d059b41029b36a1b3f7cec4035da6e9886ec417a
Федотов Илья Андреевич <ifedotov@ibs.ru>
2024-05-09 18:36:01 +0400

    v3 of the project. Add whiteList and some new features

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Image url parser
+
+simple asynchronous parser for bing image:
+### in Intellij
+1. install dependencies
+2. configurate your dataset and some parameters in config
+3. run code under async_parser_v_2.py
+
+
+### in explorer:
+1. add config.yaml and your dataset into /dist
+2. run executable
+
+to make your own executable run:
+> pyinstaller --onefile async_parser_v_2.py --hidden-import plyer.platforms.win.notification
+
+all results will store at .xlsx file in same directory. check name of your "output_file" at config<br>
+after exec wait for console closes, cause you might have little pause before file will create
--- async_parser_v_2.py
+++ async_parser_v_2.py
@@ -1,9 +1,15 @@
+if __name__ == '__main__':
+    import multiprocessing
+
+    multiprocessing.freeze_support()
+
 import asyncio
 import logging
 import re
 import urllib.parse
 import atexit
 from collections import OrderedDict
+import plyer
 
 import yaml
 from multiprocessing import Pool
@@ -23,14 +29,18 @@ config = load_config("config.yaml")
 
 def combine_columns(keywords, columns):
     strict_columns = config['strict_columns']
+    columns_offset = min(columns)
+    columns_w_offset = [col - columns_offset for col in columns]
+    strict_w_offset = [col - columns_offset for col in strict_columns]
+
     keywords_in_func = keywords.copy()
     for i in range(len(keywords_in_func)):
-        if i in strict_columns:
+        if i in strict_w_offset:
             keywords_in_func[i] = f'"{keywords_in_func[i]}"'
 
     combinations = set()
     for combination_size in range(1, len(columns) + 1):
-        for column_subset in itertools.combinations(columns, combination_size):
+        for column_subset in itertools.combinations(columns_w_offset, combination_size):
             selected_keywords = [str(keywords_in_func[i]) for i in column_subset]
             sorted_keywords = " ".join(sorted(selected_keywords))
             combinations.add(sorted_keywords)
@@ -38,14 +48,24 @@ def combine_columns(keywords, columns):
 
 
 def read_keywords_from_xlsx(filename):
-    sheet1 = openpyxl.load_workbook(filename, data_only=True)["data"]
+    sheet_name = config['sheet_name']
+    cols_to_search = config['columns']
+
+    sheet1 = openpyxl.load_workbook(filename, data_only=True)[sheet_name]
     keywords1 = []
+
+    none_value_counter = 0
     for row1 in sheet1.values:
-        row_filtered = [value for value in row1[:3] if value is not None]
-        if len(row_filtered) >= 2:
+        if none_value_counter > 5:
+            break
+        row_filtered = []
+        for col in cols_to_search:
+            if row1[col] is not None:
+                row_filtered.append(row1[col])
+        if len(row_filtered) > 0:
             keywords1.append(row_filtered)
-        if len(row_filtered) < 2:
-            return keywords1
+        else:
+            none_value_counter += 1
     return keywords1
 
 
@@ -70,7 +90,7 @@ def get_filter(shorthand):
         return ""
 
 
-async def run(row, looking_limit, adult, filter='', badsites=[]):
+async def run(row, looking_limit, adult, filter='', badsites=[], white_list=[]):
     seen = OrderedDict()
     headers = config['bing']['headers']
     columns = config['columns']
@@ -89,23 +109,24 @@ async def run(row, looking_limit, adult, filter='', badsites=[]):
                 async with session.get(request_url, headers=headers, timeout=3) as response:
                     html = await response.text()
                     for link in re.findall('murl&quot;:&quot;(.*?)&quot;', html):
+                        if any(white_site in link for white_site in white_list):
+                            seen[link] = seen.get(link, 0) + 10
+                            break
                         if any(badsite in link for badsite in badsites):
                             continue
                         seen[link] = seen.get(link, 0) + 1  # Increment occurrence count
 
             except aiohttp.ClientError as e:
                 logging.error('Error while making request to Bing: %s', e)
-
-    # Choose the link with the highest occurrence (or the first one if tied)
     best_link = max(seen, key=seen.get) if seen else ""
     return best_link
 
 
-async def process_row(row, semaphore, looking_limit, adult, badsites, filter):
+async def process_row(row, semaphore, looking_limit, adult, badsites, white_list, filter):
     async with semaphore:
         try:
             print(f"Response row: {row}")
-            link_of_the_photo = await run(row, looking_limit, adult, filter, badsites)
+            link_of_the_photo = await run(row, looking_limit, adult, filter, badsites, white_list)
             return add_column_to_row(row.copy(), link_of_the_photo)
         except Exception as e:
             logging.error(f"Error processing row {row}: {e}")
@@ -116,7 +137,8 @@ async def main():
     keywords = read_keywords_from_xlsx(config['input_file'])
     looking_limit = config['bing']['looking_limit']
     adult = config['bing']['adult_filter']
-    badsites = config['bing']['bad_sites']
+    black_list = config['bing']['black_list']
+    white_list = config['bing']['white_list']
     filter = config['bing']['filter']
 
     semaphore = asyncio.Semaphore(max_concurrent_requests)
@@ -125,22 +147,31 @@ async def main():
 
     with Pool() as pool:
         results = await asyncio.gather(*[
-            process_row(row, semaphore, looking_limit, adult, badsites, filter)
+            process_row(row, semaphore, looking_limit, adult, black_list, white_list, filter)
             for row in keywords
         ])
 
     len_keywords = len(keywords)
+    errors = 0
     for i, modified_row in enumerate(results):
         try:
             print(f"Process: {i + 1} / {len_keywords}")
             sheet.append(modified_row)
         except Exception as e:
+            errors += 1
             logging.error('Error while try bind row: %s', e)
 
     def close_file(some_workbook):
         some_workbook.save("result.xlsx")
         some_workbook.close()
 
+    plyer.notification.notify(
+        message=f'Завершено. Обработано {str(len(results))} из {str(len(keywords))}.'
+                f'ошибки: {errors}',
+        app_name='URL PARSER',
+        title='Приложение обработало все записи', )
+
+    print('Please wait for file...')
     atexit.register(close_file, workbook)
 
 
